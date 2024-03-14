d077775a2304f23aa30cf5779c08ade590664ce4
Mustafa Omar <themustafaomar@gmail.com>
2024-03-14 23:58:14 +0200

    fix: types

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'vue-composer',
-      fileName: 'vue-composer',
+      name: 'vue-compose',
+      fileName: 'vue-compose',
     },
     rollupOptions: {
       external: ['vue'],
